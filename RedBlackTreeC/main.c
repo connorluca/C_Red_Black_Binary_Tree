dfb54de8eccc55d0f4c6a0944cc31e03092964b9
connorLuca <connormccrum7@gmail.com>
2020-09-04 10:57:25 -0600

    node struct and function to create new nodes created.

---
+++ RedBlackTreeC/main.c
@@ -0,0 +1,37 @@
+/*
+ * C implementation of red balck color tree in C.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Connor McCrum
+ *
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+struct Node{
+    int data;
+    struct Node *lChild;
+    struct Node *rChild;
+};
+int main(int argc, char** argv) {
+    struct Node *root = newNode(srand()%1000);
+    //fill tree with 50 random values
+    for(int i = 0; i < 50; i++){
+        
+    }
+}
+//return pointers to memory locations and forcible allocate memory as otherwise it would fall out of scope and be lost
+struct Node * newNode(int data){
+    struct Node *node = (struct node*)malloc(sizeof(struct node)); 
+    node->data = data;
+    node->lChild = NULL;
+    node->rChild = NULL;
+    return node;
+}
