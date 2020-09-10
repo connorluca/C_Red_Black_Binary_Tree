36c5ce42b2e664fcb397e90c17ce08442693cd15
connorLuca <connormccrum7@gmail.com>
2020-09-10 00:09:10 -0600

    complete

--- RedBlackTreeC/main.c
+++ RedBlackTreeC/main.c
@@ -19,6 +19,7 @@ struct Node{
     int data;
     struct Node *lChild;
     struct Node *rChild;
+    unsigned int color: 1;// 0 for black 1 for red
 };
 int main(int argc, char** argv) {
     struct Node *root = newNode(srand()%1000);
@@ -26,13 +27,15 @@ int main(int argc, char** argv) {
     for(int i = 0; i < 50; i++){
         
     }
+    //we'll use some recursion to print out the tree 
     return 0;
 }
 //return pointers to memory locations and forcible allocate memory as otherwise it would fall out of scope and be lost
 struct Node * newNode(int data){
-    struct Node *node = (struct node*)malloc(sizeof(struct node)); 
+    struct Node *node = (struct node*)malloc(sizeof(struct node));//allocate memory and cast to the node struct type
     node->data = data;
     node->lChild = NULL;
     node->rChild = NULL;
+    node->color = 0;
     return node;
 }
